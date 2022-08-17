18560bc1eb56370e7205b982ad0228c028d54b2f
Juan Esteban Gomez Roa <jgomezr@famisanar.com.co>
2022-08-17 15:43:40 -0500

    Controller And Service - FindAll and FindById

--- src/main/java/com/ventas/ventas/service/ClienteService.java
+++ src/main/java/com/ventas/ventas/service/ClienteService.java
@@ -3,10 +3,12 @@ package com.ventas.ventas.service;
 import com.ventas.ventas.model.Cliente;
 import com.ventas.ventas.repository.IClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ClienteService implements IClienteService{
 
     @Autowired
